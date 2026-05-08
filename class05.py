b9a011d3cb5095a7480ba4f975dc403e874f4d34
Shashi Shekhar <shashishekhar.2027@gmail.com>
2026-05-08 14:02:24 +0000

    class 05

---
+++ class05.py
@@ -0,0 +1,26 @@
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+student = Student("Harry", "Gryffindor")
+print(student)
+
+#class via
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+    @property
+    def house(self):
+        return self._house
+
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
